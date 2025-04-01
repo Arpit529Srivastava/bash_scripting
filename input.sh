cbd4a36f4ff46269e2b80cba38fd116d1529cfc2
arpit529srivastava <arpitsrivastava529@gmail.com>
2025-04-02 00:28:10 +0530

    input field done

---
+++ input.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+: '
+this file covers taking input and printing it. Also using Arguments
+So what we will be doing in this is: taking input from the terminal
+like this ./input arpit srivastava
+'
+# echo "taking input continues"
+# this is also known as Arguments 
+# echo "first name :" $1 # this will print the first inputed value
+# echo "second name :" $2 # this will print the second inputed value
+# # this will print all at once
+# echo "full name:" $@
+
+
+# asking for input from the user and printing the same 
+# for this we use read command
+# echo "enter the size of the array"
+# read size
+# echo "the size of the array is $size"
+
+: '
+using of $0 
+what it does this an excellent way to self distruct a file.
+> create a file using touch command 
+> inside the use $0 to self distruct
+example code 
+'
