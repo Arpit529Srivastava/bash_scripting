0f8066566d72684f0a61a1a75dede361183f18a9
arpit529srivastava <arpitsrivastava529@gmail.com>
2025-04-01 23:20:45 +0530

    learned about printing statments
    Signed-off-by: arpit529srivastava <arpitsrivastava529@gmail.com>

---
+++ variable.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# echo "hello bash"
+# name="this is my first bash output" # this is how we define out variable
+# echo $name
+
+# printing mutltiple varibles
+
+Arpit='Arpit: `this is something to be done what you say khushi`'
+Khushi='Khushi: `whatever you are saying this is right`'
+
+echo -e "$Arpit \n$Khushi"
+# -e tag is used to print something in the next line.
